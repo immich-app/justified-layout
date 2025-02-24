0b613c7c385eef123c6e442d9e131c5aec7063a2
Mert <101130780+mertalev@users.noreply.github.com>
2025-02-24 18:00:23 +0300

    feat: bidirectional height tolerance (#10)
    bidirectional layout

--- tests/lib.rs
+++ tests/lib.rs
@@ -425,3 +425,118 @@ fn one_box_on_each_row_with_scaling() {
     assert_eq!(width3, 194);
     assert_eq!(height3, 345);
 }
+
+#[wasm_bindgen_test]
+fn add_box_to_full_row_when_it_helps() {
+    let input: Vec<f32> = vec![
+        1.5,
+        0.6666666666666666,
+        1.3274336283185841,
+        1.3333333333333333,
+        0.7516666666666667,
+        1.5,
+        0.665,
+        1.4018691588785046,
+        1.3392857142857142,
+    ];
+    let row_width = 350.0;
+    let row_height = 75.0;
+    let spacing = 4.0;
+    let height_tolerance = 0.15;
+
+    let layout = get_justified_layout(
+        input.as_slice(),
+        row_height,
+        row_width,
+        spacing,
+        height_tolerance,
+    );
+    // assert_eq!(layout, vec![]);
+    assert_eq!(layout.len(), 40);
+    let max_row_width = layout[0];
+    assert_eq!(max_row_width, 351);
+
+    let [top1, left1, width1, height1] = layout[4..8] else {
+        unreachable!()
+    };
+    assert_eq!(top1, 0);
+    assert_eq!(left1, 0);
+    assert_eq!(width1, 105);
+    assert_eq!(height1, 70);
+
+    let [top2, left2, width2, height2] = layout[8..12] else {
+        unreachable!()
+    };
+    assert_eq!(top2, 0);
+    assert_eq!(left2, width1 + spacing as i32);
+    assert_eq!(width2, 46);
+    assert_eq!(height2, 70);
+
+    let [top3, left3, width3, height3] = layout[12..16] else {
+        unreachable!()
+    };
+    assert_eq!(top3, 0);
+    assert_eq!(left3, width1 + spacing as i32 + width2 + spacing as i32);
+    assert_eq!(width3, 92);
+    assert_eq!(height3, 70);
+
+    let [top4, left4, width4, height4] = layout[16..20] else {
+        unreachable!()
+    };
+    assert_eq!(top4, 0);
+    assert_eq!(
+        left4,
+        width1 + spacing as i32 + width2 + spacing as i32 + width3 + spacing as i32 + 1
+    );
+    assert_eq!(width4, 93); // should be 92
+    assert_eq!(height4, 70);
+
+    let [top5, left5, width5, height5] = layout[20..24] else {
+        unreachable!()
+    };
+    assert_eq!(top5, height1 + spacing as i32);
+    assert_eq!(left5, 0);
+    assert_eq!(width5, 58);
+    assert_eq!(height5, 78);
+
+    let [top6, left6, width6, height6] = layout[24..28] else {
+        unreachable!()
+    };
+    assert_eq!(top6, height1 + spacing as i32);
+    assert_eq!(left6, width5 + spacing as i32);
+    assert_eq!(width6, 117);
+    assert_eq!(height6, 78);
+
+    let [top7, left7, width7, height7] = layout[28..32] else {
+        unreachable!()
+    };
+    assert_eq!(top7, height1 + spacing as i32);
+    assert_eq!(left7, width5 + spacing as i32 + width6 + spacing as i32 + 1);
+    assert_eq!(width7, 52);
+    assert_eq!(height7, 78);
+
+    let [top8, left8, width8, height8] = layout[32..36] else {
+        unreachable!()
+    };
+    assert_eq!(top8, height1 + spacing as i32);
+    assert_eq!(
+        left8,
+        width5 + spacing as i32 + width6 + spacing as i32 + width7 + spacing as i32 + 1
+    );
+    assert_eq!(width8, 109);
+    assert_eq!(height8, 78);
+
+    let [top9, left9, width9, height9] = layout[36..40] else {
+        unreachable!()
+    };
+    assert_eq!(top9, height1 + spacing as i32 + height5 + spacing as i32);
+    assert_eq!(left9, 0);
+    assert_eq!(width9, 115);
+    assert_eq!(height9, 86);
+
+    let max_row_height = layout[1];
+    assert_eq!(
+        max_row_height,
+        height1 + spacing as i32 + height5 + spacing as i32 + height9 + 1
+    );
+}
