79b422ddae8f4ba1eeb5bb12f8525222ff4be5c4
mertalev <101130780+mertalev@users.noreply.github.com>
2025-02-24 16:27:09 +0300

    bidirectional layout

--- src/lib.rs
+++ src/lib.rs
@@ -11,22 +11,24 @@ pub struct LayoutOptions {
 
 /// Given an input of aspect ratios representing boxes, returns a vector 4 times its length + 4.
 /// The first element is the maximum width across all rows, the second is the total height required
-/// to display all rows, the next two are padding, and the remaining elements are sequences of 4 
+/// to display all rows, the next two are padding, and the remaining elements are sequences of 4
 /// elements for each box, representing the top, left, width and height positions.
 /// `row_height` is a positive float that is the target height of the row.
 ///     It is not strictly followed; the actual height may be off by one due to truncation, and may be
 ///     substantially different if only one box can fit on a row and this box cannot fit with the
 ///     target height. The height cannot exceed this target unless `tolerance` is greater than zero.
 /// `row_width` is a positive float that is the target width of the row.
-///     It will not be exceeded, but a row may have a shorter width if the boxes cannot fill the row
+///     It can be exceeded by a rounding error or shorter if the boxes cannot fill the row
 ///     width given the `tolerance`.
 /// `spacing` is a non-negative float that controls the spacing between boxes, including between rows.
 ///     Notably, there is no offset applied in directions where there is no box.
 ///     The first box will have its top and left positions both at 0, not at `spacing`, and so on.
 /// `tolerance` is a non-negative float that gives more freedom to fill the row width.
 ///     When there is free space in the row and the next box cannot fit in this row, it can scale
-///     the boxes to a larger height to fill this space while respecting aspect ratios. A value of
-///     0.15 signifies that the actual row height may be up to 15% greater than the target height.
+///     the boxes to a larger height to fill this space while respecting aspect ratios. Additionally,
+///     the height can be shorter if shrinking the row height would allow more boxes to fit
+///     in the row without causing the height to be more off from the target height. A value of 0.15
+///     signifies that the actual row height may be up to 15% shorter or taller than the target height.
 ///
 /// Note: The response being Vec<i32> rather than a struct or list of structs is important, as the
 ///       JS-WASM interop is *massively* slower when moving structs to JS instead of an array and
@@ -50,25 +52,33 @@ pub fn get_justified_layout(
 }
 
 #[inline(always)]
-fn _get_justified_layout(aspect_ratios: &[f32], options: LayoutOptions) -> Vec<i32> {
+pub fn _get_justified_layout(aspect_ratios: &[f32], options: LayoutOptions) -> Vec<i32> {
     let mut positions = vec![0.0; aspect_ratios.len() * 4 + 4]; // 2 for container width and height, 2 for alignment
+    let min_row_height = options.row_height * (1.0 - options.tolerance);
     let max_row_height = options.row_height * (1.0 + options.tolerance);
-    let mut cur_row_width = 0.0;
+    let mut cur_aspect_ratio = 0.0;
+    let mut row_aspect_ratio = 0.0;
     let mut max_actual_row_width = 0.0;
     let mut row_start_idx: usize = 0;
     let mut top = 0.0;
+    let max_row_aspect_ratio = options.row_width / min_row_height;
+    let target_row_aspect_ratio = options.row_width / options.row_height;
+    let spacing_aspect_ratio = options.spacing / options.row_height;
 
-    for (i, aspect_ratio) in aspect_ratios.iter().enumerate() {
-        let box_width = aspect_ratio * options.row_height;
-        cur_row_width += box_width;
+    let mut row_diff = target_row_aspect_ratio;
+    for i in 0..aspect_ratios.len() {
+        let aspect_ratio = aspect_ratios[i];
+        cur_aspect_ratio += aspect_ratio;
+        let cur_diff = (cur_aspect_ratio - target_row_aspect_ratio).abs();
 
         // there are no more boxes that can fit in this row
-        if cur_row_width > options.row_width && i > 0 {
+        if (cur_aspect_ratio > max_row_aspect_ratio || cur_diff > row_diff) && i > 0 {
             let row = &mut positions[row_start_idx * 4 + 4..i * 4 + 4];
             let aspect_ratio_row = &aspect_ratios[row_start_idx..i];
 
             // treat the row's boxes as a single entity and scale them to fit the row width
-            let total_aspect_ratio: f32 = aspect_ratio_row.iter().sum();
+            let total_aspect_ratio =
+                row_aspect_ratio - (spacing_aspect_ratio * aspect_ratio_row.len() as f32);
             let spacing_pixels = options.spacing * f32::from(aspect_ratio_row.len() as u16 - 1);
             let scaled_row_height =
                 ((options.row_width - spacing_pixels) / total_aspect_ratio).min(max_row_height);
@@ -90,17 +100,22 @@ fn _get_justified_layout(aspect_ratios: &[f32], options: LayoutOptions) -> Vec<i
             top += scaled_row_height + options.spacing;
             max_actual_row_width = actual_row_width.max(max_actual_row_width);
             row_start_idx = i;
-            cur_row_width = box_width;
+            cur_aspect_ratio = aspect_ratio;
+            row_diff = (cur_aspect_ratio - target_row_aspect_ratio).abs();
+        } else {
+            row_diff = cur_diff;
         }
-        cur_row_width += options.spacing;
+        cur_aspect_ratio += spacing_aspect_ratio;
+        row_aspect_ratio = cur_aspect_ratio;
     }
 
     // this is the same as in the for loop and processes the last row
     // inlined because it ends up producing much better assembly
     let row = &mut positions[row_start_idx * 4 + 4..];
     let aspect_ratio_row = &aspect_ratios[row_start_idx..];
-    let total_aspect_ratio: f32 = aspect_ratio_row.iter().sum();
-    let spacing_pixels = options.spacing * f32::from(aspect_ratio_row.len() as u16 - 1);
+    let total_aspect_ratio =
+        row_aspect_ratio - (spacing_aspect_ratio * aspect_ratio_row.len() as f32);
+    let spacing_pixels = options.spacing * (aspect_ratio_row.len() as u16 - 1) as f32;
     let scaled_row_height =
         ((options.row_width - spacing_pixels) / total_aspect_ratio).min(max_row_height);
 
