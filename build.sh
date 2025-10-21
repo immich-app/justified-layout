9f473acc560cbdd07ba2239fb8fbc3e47a4977f9
mertalev <101130780+mertalev@users.noreply.github.com>
2025-10-21 14:57:24 -0400

    semicolon

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 wasm-pack build --no-pack --out-name justified-layout-wasm --target web
 echo "Uint8Array.fromBase64 ??= (string) => Uint8Array.from(atob(string), (c) => c.charCodeAt(0));" > pkg/justified-layout-wasm-module.js
-echo "export const MODULE = Uint8Array.fromBase64('$(cat pkg/justified-layout-wasm_bg.wasm | base64)')" >> pkg/justified-layout-wasm-module.js
+echo "export const MODULE = Uint8Array.fromBase64('$(cat pkg/justified-layout-wasm_bg.wasm | base64)');" >> pkg/justified-layout-wasm-module.js
