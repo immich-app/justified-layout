e7456c11b979340336b4f427a1909d9274abc965
mertalev <101130780+mertalev@users.noreply.github.com>
2025-10-21 14:56:37 -0400

    use fromBase64 with polyfill

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 wasm-pack build --no-pack --out-name justified-layout-wasm --target web
-echo "export const MODULE = '$(cat pkg/justified-layout-wasm_bg.wasm | base64)'" > pkg/justified-layout-wasm_bg.wasm.js
+echo "Uint8Array.fromBase64 ??= (string) => Uint8Array.from(atob(string), (c) => c.charCodeAt(0));" > pkg/justified-layout-wasm-module.js
+echo "export const MODULE = Uint8Array.fromBase64('$(cat pkg/justified-layout-wasm_bg.wasm | base64)')" >> pkg/justified-layout-wasm-module.js
