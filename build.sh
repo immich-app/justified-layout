235ff9b7212890415ba0b36f596774246d01275e
mertalev <101130780+mertalev@users.noreply.github.com>
2025-10-23 14:58:39 -0400

    add type declaration file

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 wasm-pack build --no-pack --out-name justified-layout-wasm --target web
+echo "export const MODULE: string;" > pkg/justified-layout-wasm-module.d.ts
 echo "Uint8Array.fromBase64 ??= (string) => Uint8Array.from(atob(string), (c) => c.charCodeAt(0));" > pkg/justified-layout-wasm-module.js
 echo "export const MODULE = Uint8Array.fromBase64('$(cat pkg/justified-layout-wasm_bg.wasm | base64 -w 0)');" >> pkg/justified-layout-wasm-module.js
